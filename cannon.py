7814eff9e910d8b48638d5c8fdb66fa3e977840a
Humberto Alejandro Rosas Téllez <93540685+Humbert100@users.noreply.github.com>
2022-05-13 14:44:46 -0500

    Cambio de velocidad

--- cannon.py
+++ cannon.py
@@ -77,8 +77,10 @@ def move():
         if not inside(target):
             #VRDL: Modificación para que el juego nunca termine
             targets[targets.index(target)].x=200
-
-    ontimer(move, 50)
+#Modifica velocidad de proyectil y objetivos
+#Entre mayor sea el número, menos rápido se moverán
+#Entre menor sea el número, más rápido se moverán
+    ontimer(move, 1) 
 
 
 setup(420, 420, 370, 0)
